fa689b36a22a79430b575f3e26f99bef469571dd
Jun Ni <90163220+DominiqueNJ@users.noreply.github.com>
2025-04-01 09:44:12 +0800

    Update exp_main.py

--- PatchTST_supervised/exp/exp_main.py
+++ PatchTST_supervised/exp/exp_main.py
@@ -299,7 +299,7 @@ class Exp_Main(Exp_Basic):
 
         mae, mse, rmse, mape, mspe, rse, corr = metric(preds, trues)
         print('mse:{}, mae:{}, rse:{}'.format(mse, mae, rse))
-        f = open("result.txt", 'a')
+        f = open("/kaggle/working/results/result.txt", 'a')
         f.write(setting + "  \n")
         f.write('mse:{}, mae:{}, rse:{}'.format(mse, mae, rse))
         f.write('\n')
